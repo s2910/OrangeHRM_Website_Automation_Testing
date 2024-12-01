f3901545437d21d37eec24b9f96ae2bea6a13eca
Acer <sewanknande123q@gmail.com>
2024-12-01 18:32:23 +0530

    init commit

--- src/test/java/Pages/Leave_Page.java
+++ src/test/java/Pages/Leave_Page.java
@@ -1,4 +1,37 @@
 package Pages;
 
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.awt.image.Kernel;
+
 public class Leave_Page {
+    WebDriver driver;
+    public Leave_Page(WebDriver ldriver){
+        this.driver=ldriver;
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(xpath = "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name'][normalize-space()='Leave']") WebElement leaveMenu;
+    @FindBy(xpath = "//a[normalize-space()='Apply']") WebElement applyMenu;
+    @FindBy(xpath = "//i[@class='oxd-icon bi-caret-down-fill oxd-select-text--arrow']") WebElement leaveType ;
+    @FindBy(xpath = "//*[contains(text(),'CAN')]") WebElement leaveTypeOption;
+    @FindBy(xpath = "//div[@class='oxd-date-input']/input") WebElement fromDate;
+    @FindBy(tagName = "textarea") WebElement comments;
+    @FindBy(xpath = "//button[@type='submit']") WebElement applyBtn;
+
+
+    public void applyLeave(String f_date,String comment) throws InterruptedException {
+        leaveMenu.click();
+        applyMenu.click();
+        leaveType.click(); //DropDown
+        leaveTypeOption.click();
+        fromDate.sendKeys(f_date);
+        comments.sendKeys(comment);
+        applyBtn.click();
+    }
 }
--- src/test/java/TestCases/Leave_Tests.java
+++ src/test/java/TestCases/Leave_Tests.java
@@ -1,20 +1,27 @@
 package TestCases;
 
 import Pages.BaseTest;
+import Pages.Leave_Page;
 import Pages.LoginPage;
 import org.testng.annotations.Test;
-
-import java.util.Base64;
 
 public class Leave_Tests extends BaseTest {
 
     @Test(priority = 1)
-    public void applyLeave(){
+    public void applyLeaveTestCase() throws InterruptedException {
+        //Login To Your Account
         LoginPage lp = new LoginPage(driver);
         String username = "Admin";
         String password = "admin123";
         lp.loginPortal(username,password);
 
+        //Apply Leave
+        Leave_Page leavePage = new Leave_Page(driver);
+        String f_Date = "2024-11-12";
+        String comment = "Leave for personal reasons";
+        leavePage.applyLeave(f_Date,comment);
+
+        //Logout From Your Account
         lp.logout();
 
      }
